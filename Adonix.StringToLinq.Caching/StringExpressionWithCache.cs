16bf0f328eac4ca58f08ceb1d746ce6ffb334d65
JasperDaDolphin <alex@mitrakis.net>
2023-08-02 19:51:58 +1000

    Cache  tests

--- Adonix.StringToLinq.Caching/StringExpressionWithCache.cs
+++ Adonix.StringToLinq.Caching/StringExpressionWithCache.cs
@@ -6,23 +6,20 @@ namespace Adonix.StringToLinq.Caching
 {
     public static class StringExpressionWithCache
     {
-        public static Func<T, bool> ToPredicate<T>(string query, MemoryCache cache, TimeSpan expiryFromNow)
+        public static (Func<T, bool> Result, bool FromCache) ToPredicate<T>(string query, MemoryCache cache, TimeSpan expiryFromNow)
         {
             var key = (typeof(T), query);
-            if (cache.TryGetValue(key, out Func<T, bool> function))
+            if (cache.TryGetValue(key, out Func<T, bool> function) && function != null)
             {
-                return function;
+                return (function, true); // Retrieved from cache
             }
-            else
+            var predicate = StringExpression.ToPredicate<T>(query).Compile();
+            cache.Set(key, predicate, new MemoryCacheEntryOptions()
             {
-                var predicate = StringExpression.ToPredicate<T>(query).Compile();
-                cache.Set(key, predicate, new MemoryCacheEntryOptions()
-                {
-                    AbsoluteExpirationRelativeToNow = expiryFromNow,
-                    Size = 1
-                });
-                return predicate;
-            }
+                AbsoluteExpirationRelativeToNow = expiryFromNow,
+                Size = 1
+            }); ;
+            return (predicate, false); // Not retrieved from cache
         }
     }
 }
--- Adonix.StringToLinq.Test/CacheTest.cs
+++ Adonix.StringToLinq.Test/CacheTest.cs
@@ -8,13 +8,12 @@ namespace Adonix.StringToLinq.Test
     public class CacheTest
     {
         [Fact]
-        public void TestCache()
+        public void TestCacheFasterThanNoCache()
         {
-
             var query = "FirstName eq \"Alex\"";
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            for (var i = 0; i < 10000; i++)
+            for (var i = 0; i < 1000; i++)
             {
                 StringExpression.ToPredicate<Employee>(query);
             }
@@ -24,13 +23,38 @@ namespace Adonix.StringToLinq.Test
 
             Stopwatch stopwatchCache = new Stopwatch();
             stopwatchCache.Start();
-            for (var i = 0; i < 10000; i++)
+            for (var i = 0; i < 1000; i++)
             {
                 StringExpressionWithCache.ToPredicate<Employee>(query, cache, TimeSpan.FromMinutes(5));
             }
             stopwatchCache.Stop();
 
             Assert.True(stopwatchCache.ElapsedMilliseconds < stopwatch.ElapsedMilliseconds);
+        }
+
+        [Fact]
+        public void TestCacheUsed()
+        {
+            var query = "FirstName eq \"Alex\"";
+            var cache = new MemoryCache(new MemoryCacheOptions());
+            var value = StringExpressionWithCache.ToPredicate<Employee>(query, cache, TimeSpan.FromMinutes(5));
+            Assert.False(value.FromCache);
+            value = StringExpressionWithCache.ToPredicate<Employee>(query, cache, TimeSpan.FromMinutes(5));
+            Assert.True(value.FromCache);
+        }
+
+        [Fact]
+        public void TestCacheUsedTimeOut()
+        {
+            var query = "FirstName eq \"Alex\"";
+            var cache = new MemoryCache(new MemoryCacheOptions());
+            var value = StringExpressionWithCache.ToPredicate<Employee>(query, cache, TimeSpan.FromSeconds(1));
+            Assert.False(value.FromCache);
+
+            Thread.Sleep(1000);
+
+            value = StringExpressionWithCache.ToPredicate<Employee>(query, cache, TimeSpan.FromSeconds(5));
+            Assert.False(value.FromCache);
         }
     }
 }
