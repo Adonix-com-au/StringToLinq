a9481c3eb9ed8fea836521baece27118cef9a838
JasperDaDolphin <alex@mitrakis.net>
2023-08-02 18:18:13 +1000

    Caching

---
+++ Adonix.StringToLinq.Caching/StringExpressionWithCache.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Adonix.StringToLinq.Caching
+{
+    public static class StringExpressionWithCache
+    {
+        public static Func<T, bool> ToPredicate<T>(string query, MemoryCache cache, TimeSpan expiryFromNow)
+        {
+            var key = (typeof(T), query);
+            if (cache.TryGetValue(key, out Func<T, bool> function))
+            {
+                return function;
+            }
+            else
+            {
+                var predicate = StringExpression.ToPredicate<T>(query).Compile();
+                cache.Set(key, predicate, new MemoryCacheEntryOptions()
+                {
+                    AbsoluteExpirationRelativeToNow = expiryFromNow,
+                    Size = 1
+                });
+                return predicate;
+            }
+        }
+    }
+}
---
+++ Adonix.StringToLinq.Test/CacheTest.cs
@@ -0,0 +1,36 @@
+﻿using Adonix.StringToLinq.Caching;
+using Xunit;
+using System.Diagnostics;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Adonix.StringToLinq.Test
+{
+    public class CacheTest
+    {
+        [Fact]
+        public void TestCache()
+        {
+
+            var query = "FirstName eq \"Alex\"";
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (var i = 0; i < 10000; i++)
+            {
+                StringExpression.ToPredicate<Employee>(query);
+            }
+            stopwatch.Stop();
+
+            var cache = new MemoryCache(new MemoryCacheOptions());
+
+            Stopwatch stopwatchCache = new Stopwatch();
+            stopwatchCache.Start();
+            for (var i = 0; i < 10000; i++)
+            {
+                StringExpressionWithCache.ToPredicate<Employee>(query, cache, TimeSpan.FromMinutes(5));
+            }
+            stopwatchCache.Stop();
+
+            Assert.True(stopwatchCache.ElapsedMilliseconds < stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
